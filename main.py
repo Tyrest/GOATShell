310a41d8ac3e60a5dd4c71bfcb69def5a0a4e11b
Aayush Goyal <Aayush@MacBook-Pro-27.lan>
2021-02-22 13:44:31 -0800

    yay first commit woohoo

---
+++ main.py
@@ -0,0 +1,16 @@
+import basic_programs
+import input_management
+import interface
+import extras
+
+def main():
+	loop()
+
+def loop():
+	# subject to change
+	while(True):
+		stdin = input("GOATSඞ ")
+
+
+if __name__ == '__main__':
+	main()
