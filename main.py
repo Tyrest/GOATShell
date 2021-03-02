771a54904dcb62d09461289c5951a3409b7eebf6
Sarah Fujimori <Sarah@Sarahs-MBP-2.attlocal.net>
2021-03-02 13:01:56 -0800

    Add to loop, write cd and pwd

--- main.py
+++ main.py
@@ -3,8 +3,41 @@ import input_management
 import interface
 import extras
 from inspect import getmembers, isfunction
+import os
+import sys
 
-functions = getmembers(basic_programs, isfunction) + getmembers(extras, isfunction)
+# Takes in complete path or end of path as argument
+# flags: none
+def cd(args, flags):
+	if args == ['..']:
+		parent = os.path.dirname(os.getcwd())
+		os.chdir(parent)
+	elif args == ['~']:
+		home = os.getenv("HOME")
+		os.chdir(home)
+	else:
+		try:
+			os.chdir(args[0])
+		except TypeError:
+			raise TypeError('Bad arguments, try again')
+		except FileNotFoundError:
+			raise FileNotFoundError('No such file or directory')
+		except:
+			raise Exception('Bad input, try again')
+
+# Returns path of current working directory
+def pwd(args, flags):
+	print(os.getcwd())
+
+def help(args, flags):
+	pass
+
+def exit(args, flags):
+	sys.exit(0)
+
+
+functions = dict(getmembers(basic_programs, isfunction) + getmembers(extras, isfunction))
+functions.update([('pwd', pwd), ('cd', cd), ('exit', exit)])
 
 def main():
 	loop()
@@ -12,24 +45,16 @@ def main():
 def loop():
 	im = input_management.input_manager(functions)
 	while(True):
-		stdin = input("GOATS ")
+		stdin = input("GOATShell: ")
 		t = im.parse(stdin)
-		break
-
-# Set up file thing??????
-# sarah
-
-def cd(args, flags):
-	pass
-
-def pwd(args, flags):
-	pass
-
-def help(args, flags):
-	pass
-
-def exit(args, flags):
-	pass
+		for fncall in t:
+			fn_name = fncall[0]; fn_args = fncall[1]; fn_flags = fncall[2]
+			try:
+				output = functions[fncall[0]](fn_args, fn_flags)
+			except KeyError:
+				print("Error: no such method")
+			except Exception as e:
+				print(type(e).__name__ + ": " + str(e.args[0]))
 
 
 if __name__ == '__main__':
