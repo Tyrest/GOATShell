fb768db75bcf7da75e7a9684ed5e45786f0257c3
Sarah Fujimori <Sarah@Sarahs-MacBook-Pro-2.local>
2021-03-10 13:33:39 -0800

    Update main loop - signals not working

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import os
 import sys
 import subprocess
 import tempfile
+import signal
+import io
+
+def signal_handler(signal, frame):
+	print("Signal")
 
 # Exit program
 def exit(args, flags):
@@ -22,6 +27,8 @@ def main():
 	while(True):
 		stdin = input("GOATS: ")
 		t = im.parse(stdin)
+		if t == None: continue
+
 		pipe_input = None
 		for fncall in t:
 			fn_name = fncall[0]; fn_args = fncall[1]; fn_flags = fncall[2]
@@ -42,13 +49,15 @@ def main():
 					pipe_input.write(output); pipe_input.seek(0)
 			except Exception as e:
 				print(type(e).__name__ + ": " + str(e.args[0]))
+		print("Current process" + str(os.getpid()))
 
 # Executes process
 def exec_process(tokens):
 	try:
+		signal.signal(signal.SIGINT, signal_handler)
 		p = subprocess.Popen(tokens, shell=False, stdin = subprocess.PIPE,stdout=subprocess.PIPE, stderr = subprocess.PIPE)
 		print("Args: " + str(p.args))
-		out, err = p.communicate(timeout=100)
+		out, err = p.communicate(timeout=1000)
 		return out
 	except subprocess.TimeoutExpired:
 		print("Timeout expired. Killing process...")
