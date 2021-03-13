c1a124700120a180966e0c6cee304148afa6bfe1
Sarah Fujimori <Sarah@Sarahs-MBP-2.attlocal.net>
2021-03-13 15:56:54 -0800

    add & support

--- input_management.py
+++ input_management.py
@@ -2,15 +2,24 @@ class input_manager:
     def __init__(self, fns):
         self.functions = fns
 
-    # Return [[function 1, arglist 1], [function 2, arglist 2]]
+    # Return (bg, [[function 1, arglist 1], [function 2, arglist 2]])
+    # where bg = True if the process should run in the background, false otherwise
     def parse(self, stdin):
         if len(stdin.strip()) == 0:
-            return None
+            return None,None
+
+        # check if the process should run in the background
+        bg = False
+        stdin = stdin.strip()
+        if stdin[-1] == '&':
+            bg = True
+            stdin = stdin[:-1]
+
         tokens = stdin.split("|")
         tokens = list(map(lambda x: x.split(), tokens))
 
         # separate fn name from args
-        return list(map(lambda x: [x[0], x[1:]], tokens))
+        return bg, list(map(lambda x: [x[0], x[1:]], tokens))
 
     # Return true if functions exist, false otherwise
     def check_input(self, input_functions):
