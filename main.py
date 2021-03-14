8f47de0f3ae1fb7f93c8bd44c1c1f4e0a97ee2cc
Sarah Fujimori <Sarah@Sarahs-MacBook-Pro-2.local>
2021-03-13 16:58:02 -0800

    Add control-z functionality, make control c and control z do nothing if sent without any processes running

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ import signal
 import io
 
 def signal_handler(sig, frame):
-	print(signal.strsignal(sig))
-	raise Exception("Signal")
+	raise Exception(signal.strsignal(sig))
+
+def signal_none(sig, frame):
+	print("\n")
+	raise Exception
 
 # Exit program
 def exit(args, flags):
@@ -26,8 +29,13 @@ def main():
 	im = input_management.input_manager(functions)
 	builtin_names = list(functions.keys())
 	while(True):
-		stdin = input("GOATS: ")
-		t = im.parse(stdin)
+		signal.signal(signal.SIGINT, signal_none)
+		signal.signal(signal.SIGTSTP, signal_none)
+		try:
+			stdin = input("GOATS: ")
+		except Exception:
+			continue
+		bg, t = im.parse(stdin)
 		if t == None: continue
 
 		pipe_input = None
@@ -37,7 +45,7 @@ def main():
 			
 			try:
 				if fn_name not in builtin_names:
-					output = exec_process([fn_name] + fn_args)
+					output = exec_process([fn_name] + fn_args, bg)
 					if pipe_input != None: pipe_input.close()
 				else:
 					output = functions[fncall[0]](fn_args, [])
@@ -52,21 +60,31 @@ def main():
 				print(type(e).__name__ + ": " + str(e.args[0]))
 
 # Executes process
-def exec_process(tokens):
+def exec_process(tokens, bg):
 	try:
 		signal.signal(signal.SIGINT, signal_handler)
-		# signal.signal(signal.SIGSTP, signal_handler)
-		p = subprocess.Popen(tokens, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		basic_programs.processes.append(p)
-		print("Args: " + str(p.args))
-		out, err = p.communicate(timeout=1000)
-		return out + err
+		signal.signal(signal.SIGTSTP, signal_handler)
+		current_process = p = subprocess.Popen(tokens, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		if bg == False:
+			out, err = p.communicate(timeout=10**3)
+			return out + err
+		else:
+			basic_programs.processes.append(p)
+			return None
+
 	except subprocess.TimeoutExpired:
 		print("Timeout expired. Killing process...")
 		p.kill()
-		out, err = p.communicate(timeout=1000)
+		out, err = p.communicate(timeout=10**3)
 		return out + err
+
 	except Exception as e:
+		if e.args[0] == signal.strsignal(signal.SIGTSTP):
+			p.send_signal(signal.SIGTSTP)
+			basic_programs.processes.append(p)
+		elif e.args[0] == signal.strsignal(signal.SIGINT):
+			p.send_signal(signal.SIGINT)
 		print(type(e).__name__ + ": " + str(e.args[0]))
 
 if __name__ == '__main__':
