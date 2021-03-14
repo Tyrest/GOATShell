3bb1374ff877a788490932a81fc46b29901a553f
Sarah Fujimori <Sarah@Sarahs-MBP-2.attlocal.net>
2021-03-14 16:58:02 -0700

    basic version of command substitution implemented

--- sarah.py
+++ sarah.py
@@ -1,12 +1,18 @@
-import signal
-import time
-
-def signal_handler(signal, frame):
+def subcommand_handler(stdin, im):
+    # find deepest occurence of $, index = len(stdin) - 1 - (first index of $ in reversed stdin)
+    index1 = len(stdin) - 1 - (stdin[::-1].index('($'))
+    index2 = stdin.index(')', index1)
+    c = stdin[index1 + 1 : index2]
     
-    return
+    # execute command 
+    output = exec_command(c, im)
+    if output is None: 
+        output = ''
 
-signal.signal(signal.SIGINT, signal_handler)
+    # replace $(command) with output
+    return stdin.replace('$(' + c + ')', output.decode('utf-8').strip())
 
-for i in range(10):
-    print(i)
-    time.sleep(3)
+s = 'cat $(echo $(echo s.py))'
+s = 'echo $(seq 1 3)'
+im = input_manager(dict())
+exec_command(subcommand_handler(s, im), im)
