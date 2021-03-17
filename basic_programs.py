f139fca3aa947f2026915a1f73cf11465138e6ae
Aayush Goyal <Aayush@MacBook-Pro-27.lan>
2021-03-16 19:28:41 -0700

    pls work

--- basic_programs.py
+++ basic_programs.py
@@ -30,8 +30,30 @@ def cd(args):
 def pwd(args):
 	print(os.getcwd())
 
-def help(args):
-	pass
+def help(args, flags): 
+	everything_dictionary = {"cd": "takes the user to a specified directory",
+							"pwd": "prints the working directory", 
+							"help": "displays a list of commands and flags and what each does", 
+							"exit": "exits the terminal", 
+							"ls": "gives the user a list of everything within the directory", 
+							"echo": "repeats user input", 
+							"jobs": "lists all active jobs", 
+							"bg": "move jobs to the background", 
+							"fg": "move jobs to the foreground", 
+
+							"SpONgeBoBtEXt": "randomly capitalizes letters in user input",
+							"meme": "generates a random meme", 
+							"mege": "makes a meme using user input", 
+							"djoosh": "makes an array for favorited songs", 
+							"autocomplete": "returns the closest named command"}
+
+
+	if args:
+		for arg in args:
+			if arg in everything_dictionary: 
+				print(arg + " " + everything_dictionary[arg])
+	else:
+		print(everything_dictionary)
 
 # Return none if arguments or flags are not valid
 def jobs(args):
